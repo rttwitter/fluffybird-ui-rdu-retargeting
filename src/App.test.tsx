129b4228f6740ba975835f8553569e63300d6dd6
Ravindra Tripathi <rtripathi@twitter.com>
2022-09-22 13:46:06 -0400

    changes for publishing to github

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-it('renders welcome message', () => {
+test('renders correctly', () => {
   render(<App />);
-  expect(screen.getByText('Learn React')).toBeInTheDocument();
+  expect(screen.getByText('Dashboard')).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,83 @@
-import React from 'react';
+import * as React from 'react';
+import Button from '@twitter/feather-component-button-react';
+import Grid from '@twitter/feather-component-grid-react';
+import Module from '@twitter/feather-component-module-react';
 import PageHeader from '@twitter/feather-component-page-header-react';
 import styles from './App.module.css';
 
-function App() {
-  return (
-    <div>
-      <PageHeader>React App</PageHeader>
-      <div className={styles.content}>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
+// Based on http://go/feather/docs/page-templates
+class App extends React.Component {
+  render() {
+    return (
+      <div className={styles.page}>
+        <nav className={styles.nav}>
+          <h3>Feather page template</h3>
+        </nav>
+
+        <PageHeader
+          endChildren={
+            <React.Fragment>
+              <Button>Secondary button</Button>
+              <Button appearance="primary">Primary button</Button>
+            </React.Fragment>
+          }
+        >
+          Dashboard
+        </PageHeader>
+
+        <div className={styles.main}>
+          <div className={styles.container}>
+            <Grid withGutter>
+              <Grid.Cell size="2/3">
+                <Module className={styles.module}>
+                  <Module.Header titleChildren="Header" />
+                  <Module.Body>
+                    <p className={styles.chart}>Content placeholder</p>
+                  </Module.Body>
+                </Module>
+              </Grid.Cell>
+
+              <Grid.Cell size="1/3">
+                <Module className={styles.module}>
+                  <Module.Header titleChildren="Header" />
+                  <Module.Body>
+                    <p className={styles.chart}>Content placeholder</p>
+                  </Module.Body>
+                </Module>
+              </Grid.Cell>
+
+              <Grid.Cell size="1/3">
+                <Module className={styles.module}>
+                  <Module.Header titleChildren="Header" />
+                  <Module.Body>
+                    <p className={styles.chart}>Content placeholder</p>
+                  </Module.Body>
+                </Module>
+              </Grid.Cell>
+
+              <Grid.Cell size="1/3">
+                <Module className={styles.module}>
+                  <Module.Header titleChildren="Header" />
+                  <Module.Body>
+                    <p className={styles.chart}>Content placeholder</p>
+                  </Module.Body>
+                </Module>
+              </Grid.Cell>
+
+              <Grid.Cell size="1/3">
+                <Module className={styles.module}>
+                  <Module.Header titleChildren="Header" />
+                  <Module.Body>
+                    <p className={styles.chart}>Content placeholder</p>
+                  </Module.Body>
+                </Module>
+              </Grid.Cell>
+            </Grid>
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
